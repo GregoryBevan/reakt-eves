d079fab2c3b53003fa2a837d7c1667cfea21b28b
Gr3gory B3van <4266007+GregoryBevan@users.noreply.github.com>
2023-04-10 20:14:45 +0200

    👷 Configure CI workflows (#6)
    * 👷 Prepare CI task
    
    * 👷 Configure gradle publish task
    
    * 👷 Print environment variable to see access to GitHub context
    
    * 👷 Try to have milestone env variable in gradle build

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+[![Events-K CI](https://github.com/GregoryBevan/events-k/actions/workflows/ci.yml/badge.svg)](https://github.com/GregoryBevan/events-k/actions/workflows/ci.yml)
+
 # event-sourcing-lib
 Simple event sourging library with Kotlin
---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    kotlin("jvm") version "1.8.20"
+    `java-library`
+    `maven-publish`
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // This dependency is exported to consumers, that is to say found on their compile classpath.
+    api("org.apache.commons:commons-math3:3.6.1")
+    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
+    implementation("com.google.guava:guava:31.1-jre")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+    doFirst {
+        println("*** ENVIRONMENT VARIABLE DUMP ***")
+        environment.forEach { (k, v) -> println("${k}:${v}") }
+
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/GregoryBevan/events-k")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "me.elgregos"
+            artifactId = "events-k"
+            version = System.getenv("github.event.milestone.title")
+
+            from(components["java"])
+        }
+    }
+}
