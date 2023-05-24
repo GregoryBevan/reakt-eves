479b2593b06aaa209651cb90c37b5633049dc80e
Grégory BÉVAN <gregory.bevan@zenika.com>
2023-05-24 09:27:39 +0200

    💚 Invert condition for snapshot repository url

--- build.gradle.kts
+++ build.gradle.kts
@@ -203,7 +203,7 @@ publishing {
     repositories {
         maven {
             name = "OSSRH"
-            url = uri(if(version.toString().endsWith("-SNAPSHOT")) "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/" else "https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            url = uri(if(version.toString().endsWith("-SNAPSHOT")) "https://s01.oss.sonatype.org/content/repositories/snapshots/" else "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/" )
             credentials {
                 username = System.getenv("MAVEN_USERNAME")
                 password = System.getenv("MAVEN_PASSWORD")
