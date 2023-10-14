79655dcd1c2cfdc5e2f271f9cbc4de9067d6e019
Gr3gory B3van <4266007+GregoryBevan@users.noreply.github.com>
2023-10-14 18:08:32 +0200

    🐛 Avoid is method serialization in domain object (#87)

--- src/main/kotlin/me/elgregos/reakteves/libs/Jsons.kt
+++ src/main/kotlin/me/elgregos/reakteves/libs/Jsons.kt
@@ -9,8 +9,10 @@ import com.fasterxml.jackson.module.kotlin.KotlinModule
 
 val genericObjectMapper: ObjectMapper = JsonMapper.builder()
     .addModules(KotlinModule.Builder().build(), JavaTimeModule())
+    .configure(MapperFeature.AUTO_DETECT_IS_GETTERS, false)
     .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
     .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+    .configure(SerializationFeature.WRITE_DURATIONS_AS_TIMESTAMPS, false)
     .configure(SerializationFeature.WRITE_DURATIONS_AS_TIMESTAMPS, false)
     .configure(MapperFeature.USE_STD_BEAN_NAMING, true)
     .build()
